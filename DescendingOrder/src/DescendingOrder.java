9015b4f9af95129d994056a1ffac309539293cd1
Oleh Voitovych <144103468+OlehVoitovych@users.noreply.github.com>
2024-03-08 16:48:54 +0200

    descending order added

---
+++ DescendingOrder/src/DescendingOrder.java
@@ -0,0 +1,21 @@
+import java.util.*;
+//https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+public class DescendingOrder {
+    private Object[] sortedNum;
+
+    public DescendingOrder(final int num) {
+        this.sortedNum = Arrays.asList(Integer.toString(num).split("")).stream().sorted(Collections.reverseOrder()).toArray();
+    }
+
+    @Override
+    public String toString() {
+        return "DescendingOrder{" +
+                "sortedNum=" + Arrays.toString(sortedNum) +
+                '}';
+    }
+
+    public static int sortDesc(final int num) {
+        return Integer.parseInt(Arrays.toString(Arrays.asList(Integer.toString(num).split("")).stream().sorted(Collections.reverseOrder()).toArray()).replace("[", "").replace("]", "").replace(",", "").replace(" ", ""));
+    }
+}
