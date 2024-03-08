54632047abecad1ffad8bac8eaeff84b1007435e
Oleh Voitovych <144103468+OlehVoitovych@users.noreply.github.com>
2024-03-08 16:48:18 +0200

    MorseCode added

---
+++ MorseCode 1/src/CustomeMorseCode.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class CustomeMorseCode {
+    public static String decode(String morseCode) {
+        // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
+        return "";
+    }
+}
