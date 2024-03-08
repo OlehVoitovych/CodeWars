d1283d3e5e2d3cb5ba61b5c465f99c12a9ad0bf8
Oleh Voitovych <144103468+OlehVoitovych@users.noreply.github.com>
2024-03-08 16:45:55 +0200

    java DeadFish added

---
+++ DeadFish/src/Test.java
@@ -0,0 +1,13 @@
+
+
+import DeadFish.DeadFish;
+
+import static org.junit.Assert.*;
+
+public class Test {
+    @org.junit.Test
+    public void tests() {
+        assertArrayEquals(new int[] {8, 64}, DeadFish.parse("iiisdoso"));
+        assertArrayEquals(new int[] {8, 64, 3600}, DeadFish.parse("iiisdosodddddiso"));
+    }
+}
